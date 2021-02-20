c939bbd3d1bdee69429a9f3c1cbe4f66a1a03a1a
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2021-02-20 11:48:12 -0600

    add prom-client dep to server

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,9 @@ import BodyParser from "body-parser";
 import express from "express";
 import { sequelize } from "./src/database/postgres";
 import { User } from "./src/models/User";
+import { collectDefaultMetrics, Registry } from "prom-client";
 
+const register = new Registry();
 const app: express.Application = express();
 
 app.use(BodyParser.json({}));
@@ -11,6 +13,11 @@ app.use(BodyParser.urlencoded({ extended: true }));
 app.get("/checkdb", async (_request, response) => {
   const auth = await sequelize.authenticate();
   response.status(200).send("Auth: " + auth);
+});
+
+app.get("/metrics", async (_req, res) => {
+  res.status(200).setHeader("Content-Type", register.contentType);
+  res.send(await register.metrics());
 });
 
 app.post("/adduser", async (request, response) => {
