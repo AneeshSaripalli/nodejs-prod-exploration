6fa170b4040934d8485d387aff460b9bfbdae1ce
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2021-02-20 15:35:20 -0600

    server reads env vars to connect to postgres instace

--- server/src/database/postgres.ts
+++ server/src/database/postgres.ts
@@ -1,7 +1,13 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("", "postgres", "secret", {
-  dialect: "postgres",
-});
+export const sequelize = new Sequelize(
+  `postgres://${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/`,
+  {
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+  }
+);
+
+//mysql://localhost:3306/database
 
 sequelize.authenticate().then(console.log).catch(console.error);
