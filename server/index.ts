66116f8bc329afc6fc1b174ef3f6bc6df763edf7
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2021-02-20 15:09:25 -0600

    log express http requests using express-winston

--- server/index.ts
+++ server/index.ts
@@ -3,12 +3,27 @@ import express from "express";
 import { sequelize } from "./src/database/postgres";
 import { User } from "./src/models/User";
 import registry from "./src/monitoring/prom";
+import winstonLogger from "express-winston";
 import { initialize_prom } from "./src/routes/prom";
+import winston from "winston";
 
 const app: express.Application = express();
 
 app.use(bodyParser.json({}));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  winstonLogger.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+    meta: true,
+    msg: "HTTP {{req.method}} {{req.url}}",
+    expressFormat: true,
+    colorize: false,
+  })
+);
 
 app.get("/checkdb", async (_request, response) => {
   const auth = await sequelize.authenticate();
